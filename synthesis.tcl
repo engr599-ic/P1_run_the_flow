54e03cf4e8d61e37801e28520031341ebba1eda6
Jack E Clarke <jackclar@sailin-research6.luddy.indiana.edu>
2025-08-04 13:45:19 -0400

    Initial Upload

---
+++ synthesis.tcl
@@ -0,0 +1,48 @@
+set TOP_MODULE picorv32
+
+set HDL_FILES {
+	./picorv32/picorv32.v
+}
+
+set MMMC_FILE ./mmmc.tcl
+
+set PDK_DIR /pdks/google/skywater-pdk/libraries/sky130_fd_pr/latest/
+set STDCELL_DIR /pdks/google/skywater-pdk/libraries/sky130_fd_sc_hs/latest/cells/
+set LIB_DIR /pdks/google/skywater-pdk/libraries/sky130_fd_sc_hs/latest/timing/
+set TECH_LEF /pdks/google/skywater-pdk/libraries/sky130_fd_pr/latest/tech/sky130_fd_pr.tlef
+
+set ALL_LEFS [glob -nocomplain -type f $STDCELL_DIR/**/*.lef]
+
+# Remove any .magic.lef files and the diode lefs.
+# .magic.lefs aren't supported by Cadence and the diode lefs are incorrect.
+set FILTERED_LEFS {} 
+foreach file $ALL_LEFS {
+	if {![string match "*.magic.lef" $file] && ![string match "*diode*" $file]} {
+		lappend FILTERED_LEFS $file
+	}
+}
+set FILTERED_LEFS [split $FILTERED_LEFS]
+
+set_db lib_search_path $LIB_DIR
+
+set_multi_cpu_usage -local_cpu 4
+
+read_mmmc $MMMC_FILE
+read_hdl $HDL_FILES
+elaborate $TOP_MODULE
+read_physical -lef [list $TECH_LEF $FILTERED_LEFS]
+init_design -top $TOP_MODULE
+set_top_module $TOP_MODULE
+
+write_db dbs/init.db
+
+syn_generic
+write_db dbs/syn_generic.db
+
+syn_map
+write_db dbs/syn_map.db
+
+syn_opt
+write_db -common dbs/syn_opt.db
+
+write_hdl > postsynth_$TOP_MODULE.v
