240b5340915f601eab96b48b78031dbcd13ee77b
Andrew Lukefahr <lukefahr@iu.edu>
2025-08-20 17:09:38 -0400

    more updates

--- pnr.tcl
+++ pnr.tcl
@@ -3,6 +3,9 @@ set_multi_cpu_usage -remote_host 1 -local_cpu 4
 set_db timing_analysis_type ocv
 set_db timing_analysis_cppr both
 
+# shoot for 50% utilization
+create_floorplan -stdcell_density_size {1.0 0.7 2 2 2 2}
+
 
 # Ensure power pins are connected to power nets
 connect_global_net VPWR -type pg_pin -pin_base_name VPWR -all
@@ -16,7 +19,8 @@ place_opt_design
 add_fillers -base_cells { sky130_fd_sc_ms__fill_1 sky130_fd_sc_ms__fi l_2 sky130_fd_sc_ms__fill_4 sky130_fd_sc_ms__fill_8 }
 write_db -common dbs/place.db
 
-ccopt_design
+clock_opt_design
+
 write_db -common dbs/ccopt.db
 
 route_opt_design
