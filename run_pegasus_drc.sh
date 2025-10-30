df42b9024b96a7d5f509763d5a4b93f08350a8e5
Andrew Lukefahr <lukefahr@iu.edu>
2025-10-30 12:10:08 -0400

    added pegasus drc script

--- run_pegasus_drc.sh
+++ run_pegasus_drc.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/bash 
+
+#fail on error
+set -e
+
+NUM_CPUS=4
 
 # Get the absolute path of the script
 SCRIPT_PATH="$(readlink -f "$0")"
@@ -9,12 +14,10 @@ WORK_DIR=${SCRIPT_DIR}/pegasus_drc
 TOP_MODULE=$(grep 'set TOP_MODULE' synthesis.tcl  | awk '{print $3}')
 echo "Running on TOP_MODULE: ${TOP_MODULE}"
 
-GDS_PATH=${WORK_DIR}/../out.gds.gz
+GDS_PATH=${SCRIPT_DIR}/${TOP_MODULE}.gds.gz
 if ! [ -e "$GDS_PATH" ]; then
   echo "ERROR:  ${GDS_PATH} does not exist."
   exit 1
-else
-  echo "Found:  ${GDS_PATH}"
 fi
 
 mkdir -p ${WORK_DIR}
@@ -32,6 +35,13 @@ echo "results_db -drc \"${REPORT_FILE}\" -ascii;" >> $CTRL_FILE
 echo "abort_on_layout_error yes;" >> $CTRL_FILE
 echo "layout_format gdsii;" >> $CTRL_FILE
 echo "layout_path \"${GDS_PATH}\";" >> $CTRL_FILE
+# Disable Density Checks
+echo "#DEFINE NODEN" >> ${CTRL_FILE}
+#Disable Guidline Rules
+echo "#DEFINE NOGLR" >> ${CTRL_FILE}
+# Disable Warnings
+echo "#DEFINE NOWARN" >> ${CTRL_FILE}
+
 
 echo "Surpressing warnings"
 unset which
@@ -40,14 +50,18 @@ unset module
 unset switchml
 unset _module_raw
 
+PEGASUS_DRC=/l/sky130_release_0.1.0/Sky130_DRC
+
 echo "Running PEGASUS DRC on ${GDS_PATH}"
 #/l/cadence/installs/PEGASUS213/bin/pegasus \
 pegasus  \
 	-top_cell ${TOP_MODULE}\
 	-ui_data \
 	--control ${CTRL_FILE} \
-	/l/sky130_release_0.0.1/Sky130_DRC/sky130_rev_0.0_1.0.drc.pvl
+	-dp ${NUM_CPUS}  \
+	/l/sky130_release_0.1.0/Sky130_DRC/sky130_rev_0.0_2.12.drc.pvl
 
+	#/l/sky130_release_0.0.1/Sky130_DRC/sky130_rev_0.0_1.0.drc.pvl
 popd
 
 if ! [ -e "$REPORT_FILE" ]; then
@@ -60,15 +74,15 @@ if ! [ -e "$SUM_FILE" ]; then
 fi
 
 echo "Checking DRC Results"
-grep 'Total DRC Results' pegasus_drc/picorv32.sum
+grep 'Total DRC Results' ${WORK_DIR}/${TOP_MODULE}.sum
 
 
-CLEAN=$(grep 'Total DRC Results' pegasus_drc/picorv32.sum  | awk '{print $5 != 0}')
+CLEAN=$(grep 'Total DRC Results' ${WORK_DIR}/${TOP_MODULE}.sum  | awk '{print $5 != 0}')
 
 if [ ${CLEAN} -ne 0 ]; then
-    echo "FAIL"
+    echo "DRC FAIL"
     exit 1
 fi
 
-echo "PASS"
+echo "DRC PASS"
 
